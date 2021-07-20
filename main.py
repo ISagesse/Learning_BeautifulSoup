eea18bd0b9e21929a52d62364b6ee058553401c0
Israel Sagesse <israelsagesse@gmail.com>
2021-07-20 19:51:38 -0400

    Scraping a live websites

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
 from bs4 import BeautifulSoup
-import lxml
+import requests
+
+respons = requests.get('https://news.ycombinator.com/')
+yc_webpage = respons.text
+soup = BeautifulSoup(yc_webpage, 'html.parser')
+
+articles = soup.find_all(name='a', class_='storylink')
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get('href')
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name='span', class_='score')]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
+
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 with open("website.html",encoding="utf8") as file:
     content = file.read()
